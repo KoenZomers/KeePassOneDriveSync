c6c796e879f4b6849db77c9cb8a3a96e21c59c49
Koen Zomers <mail@koenzomers.nl>
2015-07-02 09:28:57 +0200

    Removed plgx minimum version requirement to try to fix the issue with KeePass saying its incompatible with the latest version of KeePass

--- create-plgx.cmd
+++ create-plgx.cmd
@@ -3,7 +3,7 @@ set PLUGIN_NAME=KeeOneDriveSync
 set SourceFolder=KoenZomers.KeePass.OneDriveSync
 REM rd /s /q %~dp0src\obj %~dp0src\bin
 del %~dp0%PLUGIN_NAME%.plgx
-"C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe" --plgx-create %~dp0%SourceFolder% --plgx-prereq-kp:2.09 --plgx-prereq-net:4.0
+"C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe" --plgx-create %~dp0%SourceFolder%
 ren %~dp0%SourceFolder%.plgx %PLUGIN_NAME%.plgx
 REM zip %PLUGIN_NAME%_1_1.zip %PLUGIN_NAME%.plgx INSTALL.txt
 ECHO KeePass Plugin package has been created
