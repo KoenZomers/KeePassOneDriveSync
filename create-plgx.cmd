c65955ef9f4849e647bba4bcd6f6c16fe975f80b
Koen Zomers <mail@koenzomers.nl>
2015-12-03 14:16:58 +0100

    Changed script file to properly remove bin and obj folders to make plugin smaller

--- create-plgx.cmd
+++ create-plgx.cmd
@@ -1,9 +1,8 @@
 ECHO Creating KeePass plugin package
 set PLUGIN_NAME=KeeOneDriveSync
 set SourceFolder=KoenZomers.KeePass.OneDriveSync
-REM rd /s /q %~dp0src\obj %~dp0src\bin
+rd /s /q %~dp0%SourceFolder%\obj %~dp0%SourceFolder%\bin
 del %~dp0%PLUGIN_NAME%.plgx
-"C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe" --plgx-create %~dp0%SourceFolder% --plgx-prereq-net:3.5
+"C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe" --plgx-create %~dp0%SourceFolder% --plgx-prereq-net:4.0
 ren %~dp0%SourceFolder%.plgx %PLUGIN_NAME%.plgx
-REM zip %PLUGIN_NAME%_1_1.zip %PLUGIN_NAME%.plgx INSTALL.txt
 ECHO KeePass Plugin package has been created
