b68d3c66e339588f97450ef9d5c99833c9874d1c
KoenZomers <koen@zomers.eu>
2021-11-24 01:29:08 +0100

    Version 2.1.2.2

--- KoenZomers.KeePass.OneDriveSync/Properties/AssemblyInfo.cs
+++ KoenZomers.KeePass.OneDriveSync/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Koen Zomers")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.2.1")]
+[assembly: AssemblyVersion("2.1.2.2")]
