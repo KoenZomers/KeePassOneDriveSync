91f32f69931c27cf93c3fe0956caa2adca3177a7
Koen Zomers <koen@zomers.eu>
2023-06-22 21:15:01 +0200

    Adjusted to 64 bit KeePass path

--- create-plgx.cmd
+++ create-plgx.cmd
@@ -3,6 +3,6 @@ set PLUGIN_NAME=KeeOneDriveSync
 set SourceFolder=KoenZomers.KeePass.OneDriveSync
 rd /s /q %~dp0%SourceFolder%\obj %~dp0%SourceFolder%\bin
 del %~dp0%PLUGIN_NAME%.plgx
-"C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe" --plgx-create %~dp0%SourceFolder% --plgx-prereq-net:4.0
+"C:\Program Files\KeePass Password Safe 2\KeePass.exe" --plgx-create %~dp0%SourceFolder% --plgx-prereq-net:4.0
 ren %~dp0%SourceFolder%.plgx %PLUGIN_NAME%.plgx
 ECHO KeePass Plugin package has been created
